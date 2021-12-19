243a830ece33abe5a025a08afdccdc953307a39c
Erikajhoshi <erika.hoshino92@gmail.com>
2021-12-19 12:41:32 +1100

    Upload Files

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "85634e6262857f68d15e900d0973095e"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAUR6dc1RwARX25Fq1nA6H-CtjyO0oeU1s"
+g_key = ""
